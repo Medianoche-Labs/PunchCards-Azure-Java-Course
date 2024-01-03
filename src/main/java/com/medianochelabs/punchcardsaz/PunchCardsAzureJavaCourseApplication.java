92e476beed06c569bb907059a17ea3a17267c1da
Erikson Murrugarra <eriksonmurrugarra@Eriksons-MacBook-Pro.local>
2024-01-03 17:44:58 +0100

    Initial Commit

---
+++ src/main/java/com/medianochelabs/punchcardsaz/PunchCardsAzureJavaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.medianochelabs.punchcardsaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchCardsAzureJavaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PunchCardsAzureJavaCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medianochelabs/punchcardsaz/PunchCardsAzureJavaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medianochelabs.punchcardsaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchCardsAzureJavaCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
